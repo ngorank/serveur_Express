2f8312e212a78530cb3973f0f852a95409405d9f
ngorank <developeurkouadio@gmail.com>
2024-02-12 18:42:56 +0000

    Ajout de MYSQL

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require("express")
+const mysql = require("mysql")
 const port = process.env.PORT || 5000
-
 const app =express();
+
+ app.get("/", (req, res)=>{
+
+    
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "mydb",
+});
+
+connection.connect((err)=>{
+    if(err){
+        console.error("Erreur à la connexion à la base de données:" + err.stack)
+        return;
+    }
+    console.log("Connexion réussie")   
+})
+
+connection.query("SELECT * FROM etudiants", (err , rows, fields)=>{
+    if(err) throw err; 
+    res.json(rows)
+})
+connection.end();
+ })
+
 app.listen(port, (err)=> {
-
-app.get("/",(req, res)=>{
-    res.send('Hello World !!! ')
-} )
-
-const users = require("./routes/users")
-app.use("/users", users)
-
-    console.log("Serveur est en ligne");
+    console.log("Serveur est en ligne ");
 } )
